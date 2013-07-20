a8385064ef0287e31e45628f8631d440868af9d2
firstgeo <truenorthgeo@gmail.com>
2013-07-19 23:05:42 -0600

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var data = fs.readFileSync('index.html');
-   respose.send(dataToString('utf-8'));
+  response.send(content.toString("utf-8"));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
