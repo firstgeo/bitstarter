c701b6b68ee7560b0a226fa27405101547813e21
Ubuntu <ubuntu@ip-172-31-43-165.us-west-2.compute.internal>
2013-07-20 01:01:44 +0000

    update

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
 <<<<<<< HEAD
-  response.send('Hello to the World from the github uh huh !!!!!');
+  response.send('Hello from emacs this time!');
   
 >>>>>>> 59b80af2fdf2f31e2e10cb319f57abe6b3516023
 });
