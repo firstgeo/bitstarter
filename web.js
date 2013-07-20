d15b056ca86ec4c758ffc11224392c73d2e7bec5
Ubuntu <ubuntu@ip-172-31-43-165.us-west-2.compute.internal>
2013-07-20 01:11:10 +0000

    update web.js again

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
-  response.send('Hello from emacs this time!');
-  
->>>>>>> 59b80af2fdf2f31e2e10cb319f57abe6b3516023
+  response.send('Hello World!!!!!');
 });
 
 var port = process.env.PORT || 5000;
